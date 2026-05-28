f1ae760d23d9d97317d747c6bbef4f2a17edae63
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-27 22:42:48 -0400

    Added the login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,26 +6,26 @@ const User = require("../../models/User");
 
 //POST /api/users/register
 
-router.post("register", async (res, req)=>{
-    try{
-const { username, email, password } = req.body;
-// checking if user is an existing user
-const exitingUser = await User.findOne({ email });
-is (existingUser){
-    return res.statusCode(400).json({
-        message: "User already exists"
-    });
-}
-// hashing the password
-const hashedPassword = await bcrypt.hash(password, 10);
-// create user
-const newUser = await User.create({
-    username,
-    email,
-    password: hashedPassword
-});
+router.post("register", async (res, req) => {
+    try {
+        const { username, email, password } = req.body;
+        // checking if user is an existing user
+        const exitingUser = await User.findOne({ email });
+        is(existingUser){
+            return res.statusCode(400).json({
+                message: "User already exists"
+            });
+        }
+        // hashing the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        // create user
+        const newUser = await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
 
-          res.status(201).json({
+        res.status(201).json({
             message: "User registered successfully",
             user: {
                 _id: newUser._id,
@@ -43,3 +43,59 @@ const newUser = await User.create({
         });
     }
 });
+
+// login user POST api/users/login
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+        //comparing the stored hashed password with the newly entered password.
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+        //creating paylod
+        const payload = {
+            _id: user._id,
+            username: user.username
+        };
+
+        //sign token
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET, //process.env refers to the environment variables available to the Node.js application.
+            { expiresIn: "1h" } //process is a built-in Node.js global object
+        );
+
+        res.status(200).json({
+            message: "login successful",
+            token,
+            user: {
+                _id: user._id,
+                username: user.username,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
+});
+
+module.exports = router;
+
+
+
